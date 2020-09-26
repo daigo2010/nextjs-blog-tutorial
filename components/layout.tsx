b757e02c09289bb1fac27203f1b68057cfec1ac7
d.suganuma <ds20100531@gmail.com>
2020-09-27 04:12:55 +0900

    To TypeScript

--- components/layout.tsx
+++ components/layout.tsx
@@ -6,7 +6,13 @@ import Link from 'next/link'
 const name = 'Daigo Suganuma'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({children, home}) {
+export default function Layout({
+    children,
+    home
+}: {
+    children: React.ReactNode
+    home?: boolean
+}) {
     return (
         <div className={styles.container}>
             <Head>
@@ -16,10 +22,10 @@ export default function Layout({children, home}) {
                     content="Learn how to build a personal website using Next.js"
                 />
                 <meta
-                    propertiy="og:image"
+                    property="og:image"
                     content={`https://og-image.now.sh/${encodeURI(siteTitle)}.png?theme=lignt&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 />
-                <meta name="og:title" conteent={siteTitle} />
+                <meta name="og:title" content={siteTitle} />
                 <meta name="twitter:card" content="summary_large_image" />
             </Head>
             <header className={styles.header}>
@@ -43,6 +49,11 @@ export default function Layout({children, home}) {
                                 />
                             </a>
                         </Link>
+                        <h2 className={utilStyles.headingLg}>
+                            <Link href="/">
+                                <a className={utilStyles.colorInherit}>{name}</a>
+                            </Link>
+                        </h2>
                     </>
                 )}
             </header>
